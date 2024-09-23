ceefc90d4bd981ef96aee18cfde411e1e500dcc0
ondre86 <ondre86@gmail.com>
2024-09-23 13:16:01 -0500

    CONTACT - 1.0 (and anim, form, word fixes)

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ function $(element) {
 }
 
 addEventListener('DOMContentLoaded', ()=>{
-    intro = gsap.timeline()
-    .from(".hero-content-wrap", {
-        y:50,
-        opacity: 0,
-        duration: .75,
-        ease: "power1.inOut"
-    })
+    if (window.location.pathname == '/'){
+        intro = gsap.timeline()
+        .from(".hero-content-wrap", {
+            y:50,
+            opacity: 0,
+            duration: .75,
+            ease: "power1.inOut"
+        })
+    }
+
 })
