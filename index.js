32b4061d2621f91ae194c639e61e1609d2f1ce35
ondre86 <ondre86@gmail.com>
2024-09-23 09:21:40 -0500

    faq - 1.0 (added page directories)

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ function $(element) {
 }
 
 addEventListener('DOMContentLoaded', ()=>{
-    console.log("ji")
     intro = gsap.timeline()
     .from(".hero-content-wrap", {
         y:50,
         opacity: 0,
         duration: .75,
+        delay: .25,
         ease: "power1.inOut"
     })
 })
